c5c96e66d3411ddf7fb42dfac4e933e519ad7a71
James BARRIOS <james.barrios@larez.fr>
2017-10-08 21:53:58 +0200

    Added operator<< for NeuroneLayer

--- sources/main.cpp
+++ sources/main.cpp
@@ -8,13 +8,16 @@ int main()
 {
     // On créé une couche de neurones à 5 entrées et 5 sorties
     NeuroneLayer n(5, 5);
+    
+    cout << n << "\n";
 
     // On créé un vecteur d'entrée de dimension 5x1 valant (1, 2, 3, 4, 5)
     Eigen::VectorXf input(5);
     input << 1,2,3,4,5;
-
+    
+    
     // On affiche la sortie de la couche de neurones
-    cout << n.process(input)<< endl;
+    cout << n.process(input) << endl;
 
     return 0;
 }
--- sources/neuronelayer.cpp
+++ sources/neuronelayer.cpp
@@ -4,14 +4,20 @@ NeuroneLayer::NeuroneLayer(unsigned int inputSize, unsigned int outputSize, std:
 : mPoids(Eigen::MatrixXf::Random(outputSize, inputSize))
 , mActivationFun(activationF)
 {
-    std::cout << mPoids << std::endl;
 }
 
 Eigen::VectorXf NeuroneLayer::process(Eigen::VectorXf inputs) const
 {
     Eigen::VectorXf activationLevel = mPoids*inputs;
+    
     for(unsigned int i(0); i < activationLevel.size(); i++)
         activationLevel[i] = mActivationFun(activationLevel[i]);
 
     return activationLevel;
+}
+
+std::ostream& operator<<(std::ostream& flux, NeuroneLayer n)
+{
+    flux << n.mPoids;
+    return flux;
 }
