8ee229041be1d46eea3a350f38e07d61f56489fd
Manu-Tran <emmanuel.tran@gmail.com>
2017-10-13 16:50:39 +0200

    Une amélioration et une condition d'arrêt pour garantir la convegence.

--- sources/teacher.cpp
+++ sources/teacher.cpp
@@ -19,9 +19,21 @@ void Teacher::backProp(Eigen::VectorXf input, Eigen::VectorXf desiredOutput, flo
 
 void Teacher::propError(Eigen::VectorXf xnPartialDerivative, float step)
 {
-    for(auto itr = mNetwork->rbegin(); itr != mNetwork->rend(); ++itr)
+    float epsilon = 0.02; //Paramètre à fixer
+
+    auto max = xnPartialDerivative(0);
+    for(auto i(1); i<xnPartialDerivative.size(); i++)
     {
-        xnPartialDerivative = itr->backProp(xnPartialDerivative, step);
+        if (max < xnPartialDerivative(i))
+           max = xnPartialDerivative(i);
+    }
+
+    if (max < epsilon)
+    {
+        for(auto itr = mNetwork->rbegin(); itr != mNetwork->rend(); ++itr)
+        {
+            xnPartialDerivative = itr->backProp(xnPartialDerivative, step);
+        }
     }
 }
 
@@ -29,9 +41,11 @@ Eigen::VectorXf Teacher::errorVector(Eigen::VectorXf output, Eigen::VectorXf des
 {
     Eigen::VectorXf errorVect = Eigen::MatrixXf::Zero(output.size(), 1);
 
+    Eigen::VectorXf deltaX(Eigen::MatrixXf::Zero(output.size(), 1));
+
     for(unsigned int i(0); i < output.size(); ++i)
     {
-        Eigen::VectorXf deltaX(Eigen::MatrixXf::Zero(output.size(), 1));
+        deltaX((i-1)%output.size()) = 0;
         deltaX(i) = dx;
         errorVect(i) = (mErrorFun(output + deltaX, desiredOutput) - mErrorFun(output, desiredOutput))/dx;
     }
