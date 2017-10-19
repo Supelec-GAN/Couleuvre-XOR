7165dd536506b4980667641d07e4ec9914c404de
Ptichapron <benjamin.mouscadet@supelec.fr>
2017-10-19 11:50:01 +0200

    Amélioration des constructeurs de NeuralNetwork pour les rendre plus légers

--- headers/neuralnetwork.hpp
+++ headers/neuralnetwork.hpp
@@ -3,7 +3,9 @@
 
 #include <list>
 #include <vector>
+
 #include "neuronlayer.hpp"
+#include "headers/functions.hpp"
 
 class NeuralNetwork : public std::list<NeuronLayer>
 {
@@ -12,31 +14,20 @@ class NeuralNetwork : public std::list<NeuronLayer>
 
     public:
 	
-	/// Constructeur permettant d'initialiser une réseau neuronal vide
+        /// Constructeur permettant d'initialiser une réseau neuronal vide
         NeuralNetwork();
 	
-    /// Constructeur permettant d'initialiser un réseau neuronal complet
-    /**
-     * \param nbLayer le nombre de couches du réseau
-	 * \param nbInputs ne nombre d'entrées du réseau
-     * \param arrayNbNeuronsPerLayer la liste du nombre de neurone par couche (nbLayer éléments)
-     * \param arrayActivationFunctionPerLayer la liste des fonctions d'activation de chaque couche (nbLayer éléments)
-	 */
-        NeuralNetwork(unsigned int nbLayer, unsigned int nbInputs, std::vector<unsigned int> arrayNbNeuronsPerLayer, std::vector<std::function<float(float)>> arrayActivationFunctionPerLayer);
+        /// Constructeur permettant d'initialiser un réseau neuronal complet
+        NeuralNetwork(std::vector<unsigned int> layerSizes, std::vector<Functions::ActivationFun> activationFuns);
 	
-	/// Constructeur permettant d'initialiser un réseau neuronal avec la fonction par défaut
-	/**
-	 * \param nbLayer le nombre de couches du réseau
-	 * \param nbInputs ne nombre d'entrées du réseau
-	 * \param arrayNbNeuronsPerLayer la liste du nombre de neurone par couche (nbLayer éléments)
-	 */
-        NeuralNetwork(unsigned int nbLayer, unsigned int nbInputs, std::vector<unsigned int> arrayNbNeuronsPerLayer);
+        /// Constructeur permettant d'initialiser un réseau neuronal avec la fonction par défaut
+         NeuralNetwork(std::vector<unsigned int> layerSizes);
 
-    /// Constructeur permettant d'initialiser le réseau neuronal avec un conteneur de neuronLayer
-    /**
-     *  \param Container un conteneur (vector, list...) de NeuronLayer
-     *  \param layerList la liste des couches de neurones
-     */
+        /// Constructeur permettant d'initialiser le réseau neuronal avec un conteneur de neuronLayer
+        /**
+         *  \param Container un conteneur (vector, list...) de NeuronLayer
+         *  \param layerList la liste des couches de neurones
+         */
         template <typename Container>
         NeuralNetwork(Container layerList);
 
@@ -44,11 +35,11 @@ class NeuralNetwork : public std::list<NeuronLayer>
 
 
     public:
-    /// Fonction utilitaire permettant d'afficher le réseau de neurones
-    /**
-     * Cette fonction affiche les matrices de poids des différents layers du réseau
-     */
-    friend std::ostream& operator<<(std::ostream& flux, NeuralNetwork network);
+        /// Fonction utilitaire permettant d'afficher le réseau de neurones
+        /**
+         * Cette fonction affiche les matrices de poids des différents layers du réseau
+         */
+        friend std::ostream& operator<<(std::ostream& flux, NeuralNetwork network);
 
 };
 
--- sources/neuralnetwork.cpp
+++ sources/neuralnetwork.cpp
@@ -2,35 +2,20 @@
 
 NeuralNetwork::NeuralNetwork(){}
 
-
-NeuralNetwork::NeuralNetwork(unsigned int nbLayer, unsigned int nbInputs, std::vector<unsigned int> arrayNbNeuronsPerLayer, std::vector<std::function<float(float)>> arrayActivationFunctionPerLayer)
+NeuralNetwork::NeuralNetwork(std::vector<unsigned int> layerSizes, std::vector<Functions::ActivationFun> activationFuns)
 {
-    push_back(NeuronLayer(  nbInputs,
-                            arrayNbNeuronsPerLayer[0],
-                            arrayActivationFunctionPerLayer[0]
-                            )) ;
-	
-    for(unsigned int i=0; i < nbLayer - 1; ++i)
-	{
-        push_back(NeuronLayer(      arrayNbNeuronsPerLayer[i],
-									arrayNbNeuronsPerLayer[i+1],
-									arrayActivationFunctionPerLayer[i+1]
-									));
-	}
+    if(layerSizes.size() != activationFuns.size() + 1)
+        throw std::logic_error("NeuralNetwork::NeuralNetwork error - Sizes of parameters do not match");
+
+    for(size_t i(0); i < layerSizes.size()-1; ++i)
+        push_back(NeuronLayer(layerSizes[i], layerSizes[i+1], activationFuns[i+1]));
 }
 
-NeuralNetwork::NeuralNetwork(unsigned int nbLayer, unsigned int nbInputs, std::vector<unsigned int> arrayNbNeuronsPerLayer)
+
+NeuralNetwork::NeuralNetwork(std::vector<unsigned int> layerSizes)
 {
-    push_back(NeuronLayer(  nbInputs,
-                            arrayNbNeuronsPerLayer[0]
-                            )) ;
-	
-	for(unsigned int i=0; i < nbLayer - 1; ++i)
-	{
-        push_back(NeuronLayer(  arrayNbNeuronsPerLayer[i],
-                                arrayNbNeuronsPerLayer[i+1]
-                                ));
-	}
+    for(size_t i(0); i < layerSizes.size()-1; ++i)
+        push_back(NeuronLayer(layerSizes[i], layerSizes[i+1]));
 }
 
 
