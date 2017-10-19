90509538e35e2b652a8713d24ecf30475d7679f7
Ptichapron <benjamin.mouscadet@supelec.fr>
2017-10-19 16:38:47 +0200

    ajout de la fonction tangente hyperbolique

--- sources/functions.cpp
+++ sources/functions.cpp
@@ -12,6 +12,11 @@ Functions::ActivationFun Functions::heavyside(float gapAbscissa)
     return [=] (float x) {return (x < gapAbscissa) ? 0 : 1;};
 }
 
+Functions::ActivationFun Functions::hyperTan()
+{
+    return [=] (float x) {return tanh(2*x/3);};
+}
+
 Functions::ErrorFun Functions::l2Norm()
 {
     return [] (Eigen::VectorXf v1, Eigen::VectorXf v2) {return (v1-v2).squaredNorm();};
