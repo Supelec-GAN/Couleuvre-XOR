787f1d5baa4c86ba1e5235cf3116a1896e9535e3
Ptichapron <benjamin.mouscadet@supelec.fr>
2017-10-23 19:42:41 +0200

    Regénération de la documentation

--- Documentation/html/search/all_0.js
+++ Documentation/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['backprop',['backProp',['../classNeuronLayer.html#a0896580aa265681f77efbcb81c6c8150',1,'NeuronLayer::backProp()'],['../classTeacher.html#a038afb323ffc518a3a3af5044f931442',1,'Teacher::backProp()']]]
+  ['backprop',['backProp',['../classNeuronLayer.html#a0896580aa265681f77efbcb81c6c8150',1,'NeuronLayer::backProp()'],['../classTeacher.html#a99fc69c5319be890394d2c8503e217c8',1,'Teacher::backProp()']]]
 ];
