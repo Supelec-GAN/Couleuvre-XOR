8a271392dad35763d2ddc8bacb9488a4a8f5bcea
Ptichapron <benjamin.mouscadet@supelec.fr>
2017-10-27 09:28:58 +0200

    Correction d'un problème d'indentation

--- sources/main.cpp
+++ sources/main.cpp
@@ -14,54 +14,56 @@ using namespace std;
 
 int main()
 {   
-   try
-   {
-       // Construction du générateurs d'inputs
-       std::default_random_engine generator((std::random_device())());
-       std::uniform_real_distribution<float> distribution(-1.f,1.f);
+    try
+    {
 
-       // Construction du réseau de neurones
-       std::vector<unsigned int> sizes{ {2,2,2,1} };
-       std::vector<Functions::ActivationFun> funs{ {Functions::sigmoid(3.f), Functions::sigmoid(3.f), Functions::sigmoid(3.f)} };
-       std::shared_ptr<NeuralNetwork> network(new NeuralNetwork(sizes, funs));
+        // Construction du générateurs d'inputs
+        srand(time(0));
+        std::default_random_engine generator((std::random_device())());
+        std::uniform_real_distribution<float> distribution(-1.f,1.f);
 
-       // Construction du batch d'exemples de test
-       Application::Batch testBatch;
-       for(size_t i{0}; i < 1000000; i++)
-       {
-           Eigen::VectorXf input(2);
-           Eigen::VectorXf output(1);
-           input(0)  = distribution(generator);
-           input(1)  = distribution(generator);
-           output(0) = ((input(0) >= 0) ^ (input(1) >= 0)) ? 1 : 0;
+        // Construction du réseau de neurones
+        std::vector<unsigned int> sizes{ {2,2,2,1} };
+        std::vector<Functions::ActivationFun> funs{ {Functions::sigmoid(3.f), Functions::sigmoid(3.f), Functions::sigmoid(3.f)} };
+        std::shared_ptr<NeuralNetwork> network(new NeuralNetwork(sizes, funs));
 
-           Application::Sample sample(input, output);
-           testBatch.push_back(sample);
-       }
+        // Construction du batch d'exemples de test
+        Application::Batch testBatch;
+        for(size_t i{0}; i < 1000000; i++)
+        {
+            Eigen::VectorXf input(2);
+            Eigen::VectorXf output(1);
+            input(0)  = distribution(generator);
+            input(1)  = distribution(generator);
+            output(0) = ((input(0) >= 0) ^ (input(1) >= 0)) ? 1 : 0;
 
-       // Construction du batch d'exemples d'apprentissage
-       Application::Batch teachBatch;
-       for(size_t i{0}; i < 1000000; i++)
-       {
-           Eigen::VectorXf input(2);
-           Eigen::VectorXf output(1);
-           input(0)  = distribution(generator);
-           input(1)  = distribution(generator);
-           output(0) = ((input(0) >= 0) ^ (input(1) >= 0)) ? 1 : 0;
+            Application::Sample sample(input, output);
+            testBatch.push_back(sample);
+        }
+
+        // Construction du batch d'exemples d'apprentissage
+        Application::Batch teachBatch;
+        for(size_t i{0}; i < 1000000; i++)
+        {
+            Eigen::VectorXf input(2);
+            Eigen::VectorXf output(1);
+            input(0)  = distribution(generator);
+            input(1)  = distribution(generator);
+            output(0) = ((input(0) >= 0) ^ (input(1) >= 0)) ? 1 : 0;
 
 
-           Application::Sample sample(input, output);
-           teachBatch.push_back(sample);
-       }
+            Application::Sample sample(input, output);
+            teachBatch.push_back(sample);
+        }
 
-       //Construction de l'application qui gère tout
-       Application appXOR(network, teachBatch, testBatch);
+        //Construction de l'application qui gère tout
+        Application appXOR(network, teachBatch, testBatch);
 
-       appXOR.totalRun(100, 1000, 3000);
-   }
-   catch (const std::exception& ex)
-   {
-       std::cout << "Exception was thrown: " << ex.what() << std::endl;
-   }
-   return 0;
+        appXOR.totalRun(100, 1000, 3000);
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << "Exception was thrown: " << ex.what() << std::endl;
+    }
+    return 0;
 }
