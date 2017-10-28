2b0001b3871a1563d12af98c83f7135048d3a413
Ptichapron <benjamin.mouscadet@supelec.fr>
2017-10-28 16:17:48 +0200

    Construction de errorcollector, qui sert juste à stocker les données et à les traiter lorsque nécessaire

--- headers/errorcollector.hpp
+++ headers/errorcollector.hpp
@@ -1,13 +1,33 @@
 #ifndef ERRORCOLLECTOR_HPP
 #define ERRORCOLLECTOR_HPP
 
+#include <vector>
+
 namespace Stats
 {
 
 class ErrorCollector
 {
     public:
-        ErrorCollector();
+        struct StatisticData
+        {
+            float mean;
+            float deviation;
+            float confidenceRange;
+        };
+
+    public:
+                        ErrorCollector();
+
+        StatisticData   processData() const;
+        void            addResult(float result);
+
+    private:
+
+
+    private:
+        std::vector<float> mErrors;
+
 };
 
 }
--- sources/errorcollector.cpp
+++ sources/errorcollector.cpp
@@ -1,11 +1,30 @@
 #include "headers/errorcollector.hpp"
 
-namespace Stats
-{
+#include <numeric>
+#include <algorithm>
+#include <math.h>
 
-ErrorCollector::ErrorCollector()
+Stats::ErrorCollector::ErrorCollector()
 {
 
 }
 
+Stats::ErrorCollector::StatisticData Stats::ErrorCollector::processData() const
+{
+    StatisticData data;
+
+    data.mean = std::accumulate(mErrors.begin(), mErrors.end(), 0.f)/(static_cast<float>(mErrors.size()));
+
+    float deviation(0);
+    std::for_each(mErrors.begin(), mErrors.end(), [&] (float x) {deviation += x*x;});
+    data.deviation = sqrt(deviation/static_cast<float>(mErrors.size()) - pow(data.mean, 2));
+
+    data.confidenceRange = 2*data.deviation/(sqrt(static_cast<float>(mErrors.size())));
+
+    return data;
+}
+
+void Stats::ErrorCollector::addResult(float result)
+{
+    mErrors.push_back(result);
 }
